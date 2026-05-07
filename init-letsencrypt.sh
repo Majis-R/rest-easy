f0417c87d0a8f4e6a1cbeea8153584791ab6a9e5
Rei <majaristo@gmail.com>
2026-05-07 21:26:34 +0300

    Fix init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,14 +12,17 @@ fi
 
 echo "### Cleaning up old containers to prevent ContainerConfig errors ..."
 docker-compose down --remove-orphans || true
+# Kill and remove everything just to be safe from the bug
+docker ps -a --format "{{.Names}}" | grep bubly | xargs -r docker rm -f || true
 echo
 
 echo "### Downloading recommended TLS parameters if missing ..."
 docker-compose run --rm --entrypoint "sh -c '\
   if [ ! -s /etc/letsencrypt/options-ssl-nginx.conf ] || [ ! -s /etc/letsencrypt/ssl-dhparams.pem ]; then \
     mkdir -p /etc/letsencrypt; \
-    curl -L -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -o /etc/letsencrypt/options-ssl-nginx.conf
-    curl -L -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem -o /etc/letsencrypt/ssl-dhparams.pem  fi'" certbot
+    curl -L -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -o /etc/letsencrypt/options-ssl-nginx.conf; \
+    curl -L -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem -o /etc/letsencrypt/ssl-dhparams.pem; \
+  fi'" certbot
 
 # Check if certificates exist already in the Docker volume
 cert_exists=$(docker-compose run --rm --entrypoint "sh -c 'if [ -d /etc/letsencrypt/live/${domains[0]} ]; then echo 1; else echo 0; fi'" certbot | tr -d '\r')
@@ -81,5 +84,6 @@ echo "### Starting the rest of the application stack ..."
 # Re-apply the ContainerConfig workaround to the main stack
 docker-compose rm -f -s api || true
 docker-compose rm -f -s db || true
+docker-compose rm -f -s nginx || true
 docker-compose rm -f -s certbot || true
 docker-compose up -d --build
